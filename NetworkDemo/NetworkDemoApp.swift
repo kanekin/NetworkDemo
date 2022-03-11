dad5ee0da829c0b112c2e36bffd6d5d93e2ef152
Tatsuya Kaneko <tatsuya.j.kaneko@gmail.com>
2022-03-11 10:20:31 +0100

    Initial Commit

---
+++ NetworkDemo/NetworkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkDemoApp.swift
+//  NetworkDemo
+//
+//  Created by Tatsuya Kaneko on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
