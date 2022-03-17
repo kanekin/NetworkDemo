cf3866323408e3fbad9ae9c97295b5cda8d75f16
Tatsuya Kaneko <tatsuya.j.kaneko@gmail.com>
2022-03-17 10:58:53 +0100

    Improved a look of the login and signin screen

--- NetworkDemo/Features/LoginScreen/LoginScreen.swift
+++ NetworkDemo/Features/LoginScreen/LoginScreen.swift
@@ -12,60 +12,60 @@ struct LoginScreen: View {
     @State var password: String = ""
     
     var body: some View {
-        ZStack {
-            VStack {
-                Spacer()
-                
-                Text("Sign in to Kinema Record")
-                    .foregroundColor(.white)
-                    .font(.title2)
-                    .fontWeight(.bold)
-                
-                VStack(alignment: .leading, spacing: 0.0) {
-                    TextField("Email", text: $email)
-                        .frame(minHeight: 42.0)
-                        .background(Color.systemBackground)
-                        .textFieldStyle(.plain)
-                    Divider()
-                    TextField("Password", text: $password)
-                        .frame(minHeight: 42.0)
-                        .background(Color.systemBackground)
-                        .textFieldStyle(.plain)
+        VStack {
+            Image("OneFlewOverTheCuckoosNest")
+                .resizable()
+                .clipped()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 350, alignment: .bottom)
+                .clipped()
+                .overlay {
+                    LinearGradient(gradient: Gradient(colors: Color.backgroundGradient), startPoint: .top, endPoint: .bottom)
                 }
-                .padding(.init(top: 0.0, leading: 16.0, bottom: 0.0, trailing: 16.0))
-                .background(Color.systemBackground)
-                
-                Button(action: {}) {
-                    Text("Login")
-                        .fontWeight(.semibold)
-                        .frame(maxWidth: .infinity)
+                .overlay {
+                    Text("Sign in to Kinema Record")
+                        .foregroundColor(.label)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .frame(maxHeight: .infinity, alignment: .bottom)
+                        .padding()
                 }
-                .tint(.green)
-                .buttonStyle(.borderedProminent)
-                .buttonBorderShape(.roundedRectangle(radius: 5))
-                .controlSize(.large)
-                .padding(.init(top: 8.0, leading: 16.0, bottom: 16.0, trailing: 16.0))
-                
-                Spacer()
+            
+            VStack(alignment: .leading, spacing: 0.0) {
+                TextField("Email", text: $email)
+                    .frame(minHeight: 42.0)
+                    .background(Color.systemBackground)
+                    .textFieldStyle(.plain)
+                Divider()
+                TextField("Password", text: $password)
+                    .frame(minHeight: 42.0)
+                    .background(Color.systemBackground)
+                    .textFieldStyle(.plain)
             }
-            .background {
-                Image("OneFlewOverTheCuckoosNest")
-                    .resizable()
-                    .clipped()
-                    .aspectRatio(contentMode: .fill)
-                    .overlay {
-                        LinearGradient(gradient: Gradient(colors: Color.gradientFromCenter), startPoint: .top, endPoint: .bottom)
-                    }
+            .padding(.init(top: 0.0, leading: 16.0, bottom: 0.0, trailing: 16.0))
+            .background(Color.systemBackground)
+            
+            Button(action: {}) {
+                Text("Login")
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity)
             }
+            .tint(.green)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 5))
+            .controlSize(.large)
+            .padding(.init(top: 8.0, leading: 16.0, bottom: 16.0, trailing: 16.0))
+            
+            Spacer()
         }
         .ignoresSafeArea(edges: [.top])
-        .background(Color.black)
+        .background(Color.systemBackground)
     }
 }
 
 struct LoginScreen__Previews: PreviewProvider {
     static var previews: some View {
         LoginScreen()
-            .preferredColorScheme(.dark)
+//            .preferredColorScheme(.dark)
     }
 }
